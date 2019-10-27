22c38ea35c36fedda8e56334a9b4887dfb8f88ad
Kotov Alexander <kotov.alexander@mail.ru>
2019-10-27 23:34:39 +0300

    Lesson_8

---
+++ Lesson-8/task8.js
@@ -0,0 +1,47 @@
+function Animal(name) {
+    this.name = name;
+    var self = this;
+
+    var foodAmount = 50;
+
+    function formatFoodAmount() {
+        return foodAmount + ' гр.';
+    }
+
+    self.dailyNorm = function(amount) {
+        if (!arguments.length) return formatFoodAmount();
+
+        if (amount < 50 || amount > 500) {
+            return 'Недопустимое количество корма.';
+        }
+
+        foodAmount = amount;
+    };
+
+    this.feed = function() {
+        console.log('Насыпаем в миску ' + self.dailyNorm() + ' корма.');
+    };
+}
+
+function Cat(name) {
+    Animal.apply(this, arguments);
+
+    var animalFeed = this.feed;
+    this.feed = function() {
+        animalFeed(); return this;
+        this.run();
+    };
+    this.run = function () {
+        console.log('Кот доволен ^_^'); return this;
+    };
+    this.stroke = function () {
+        console.log('Гладим кота.'); return this;
+    };
+}
+
+var barsik = new Cat('Барсик');
+
+barsik.stroke().feed().run();
+barsik.run().feed().stroke();
+
+barsik = null;
